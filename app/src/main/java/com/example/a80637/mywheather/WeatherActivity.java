bbd4903e74c5ca016b4d77080354a79ae2d39b56
806371a <806371062@qq.com>
2019-04-08 20:14:04 +0800

    加入了自动更新，更改了图标与名字

--- app/src/main/java/com/example/a80637/mywheather/WeatherActivity.java
+++ app/src/main/java/com/example/a80637/mywheather/WeatherActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.a80637.mywheather.gson.Forecast;
 import com.example.a80637.mywheather.gson.Weather;
+import com.example.a80637.mywheather.service.AutoUpdateService;
 import com.example.a80637.mywheather.util.HttpUtil;
 import com.example.a80637.mywheather.util.Utility;
 
@@ -193,6 +193,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
